f42ded81fedae8034ec0b99fd7568ee2773d8212
Dustin Sparks <pixelmixer@gmail.com>
2018-07-05 22:16:27 -0400

    Made addition code more flexible
    Added a couple of new tests.

--- src/sum.test.js
+++ src/sum.test.js
@@ -5,3 +5,18 @@ test('Sum correctly adds two basic numbers', () => {
 });
 
 test('Sum adds 80 to 20 and results in 100', () => expect(sum(80, 20)));
+
+test('Sum adds an arbitrary amount of numbers', () => {
+  const count = Math.random() * 1000;
+  const arr = [];
+  let accum = 0;
+
+  for (let i = 0; i < count; i += 1) {
+    arr[i] = i;
+    accum += i;
+  }
+
+  expect(sum(...arr)).toEqual(accum);
+});
+
+test('Two and two is two... right?', () => expect(2).toEqual(3));
