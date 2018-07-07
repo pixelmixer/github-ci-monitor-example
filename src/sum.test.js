e7999fb3a0b8ba9665040d8f3f17455f96d3fd34
Dustin Sparks <pixelmixer@gmail.com>
2018-07-06 21:11:33 -0400

    Fixed broken test cases.

--- src/sum.test.js
+++ src/sum.test.js
@@ -4,8 +4,6 @@ test('Sum correctly adds two basic numbers', () => {
   expect(sum(2, 2)).toBe(4);
 });
 
-test('Sum adds 80 to 20 and results in 100', () => expect(sum(80, 20)));
-
 test('Sum adds an arbitrary amount of numbers', () => {
   const count = Math.random() * 1000;
   const arr = [];
@@ -19,12 +17,6 @@ test('Sum adds an arbitrary amount of numbers', () => {
   expect(sum(...arr)).toEqual(accum);
 });
 
-test('Two and two is two... right?', () => expect(2).toEqual(3));
-
 test('Sums are calculated consistently', () => {
   expect(sum(4, 5, 6, 7, 8)).toBeGreaterThan(sum(1, 2, 3, 4, 5));
-});
-
-test('Sums are calculated inconsistently', () => {
-  expect(sum(4, 5, 6, 7, 8)).toBeGreaterThan(sum(100, 2, 3, 4, 5));
 });
